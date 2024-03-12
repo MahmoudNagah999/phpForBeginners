c21190132df6d43deaef78923fa96fc31046176e
MahmoudNagah999 <Mahmoudnagah999@gmail.com>
2024-03-12 23:06:15 +0200

    PDO connection

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
 
 require "helper.php";
+
 require "router.php";
 
 // connect to the Database Using PDO (php Data Object)
+$dsn = "mysql:host=localhost;port=3306;dbname=Demo;charset=utf8mb4"; //  data source name
+
+$pdo  = new PDO($dsn, "root", "");
+
+$query = $pdo->prepare('select * from Posts');    
+$query->execute();
+$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($posts as $post){
+    echo "<li>" .$post['title']."</li>";
+}
+
+     
